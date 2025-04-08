c5a0aa72df61ae8cc47e136f5559d7635cac60c0
VIGNESHK2883 <113919523+VIGNESHK2883@users.noreply.github.com>
2025-04-08 15:51:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import AddDevice from './pages/AddDevice'
-import Login from './pages/Login'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddDevicePage from './AddDevicePage';
+import LoginPage from './LoginPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AddDevice />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<AddDevicePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
